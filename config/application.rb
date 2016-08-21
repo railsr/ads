e6823cf0c0f7076e0191a95b0a88b98e900690c7
railsr <ikll7@yahoo.com>
2016-08-22 03:19:46 +0400

    Cors

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,13 @@ module Ads
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # 'change this to real domain in production. localhost:3000'
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end

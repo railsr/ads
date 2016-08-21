67dbc5a707dc744f31feea2eb0ab25be5bb4e206
railsr <ikll7@yahoo.com>
2016-08-22 00:35:42 +0400

    fx

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(name: params[:name],
-                    email: params[:email],
-                    password: params[:password])
+    user = User.new(user_params)
     if user.save
       render json: user, status: :created
     else
@@ -20,5 +18,9 @@ class UsersController < ApplicationController
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end

197d393a819df426137c070c69629b3319b29fb9
railsr <ikll7@yahoo.com>
2016-09-01 22:36:08 +0400

    serializers + components update

--- app/serializers/ad_serializer.rb
+++ app/serializers/ad_serializer.rb
@@ -1,3 +1,8 @@
 class AdSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :created_at
+  include ActionView::Helpers::DateHelper
+  attributes :id, :title, :description, :created
+
+  def created
+    time_ago_in_words(object.created_at) + " ago"
+  end
 end
--- ng-client/src/app/ads/ad.component.ts
+++ ng-client/src/app/ads/ad.component.ts
@@ -5,10 +5,11 @@ import { Component, Input } from '@angular/core';
   template: `
   <div class="media wellc">
     <div class="media-left">
-
+      
     </div>
     <div class="media-body">
       <h4 class="media-heading"><a [routerLink]="['/ads', data.id]">{{data.attributes.title}}</a> </h4>
+      <span class="">{{data.attributes.created}}</span>
       <p [innerHTML] = "data.attributes.description | md | truncate:data.id"></p>
     </div>
   </div>
